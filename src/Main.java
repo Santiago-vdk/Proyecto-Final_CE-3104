d06221fc6691b3c533455498522864ece2d34c45
Rafael Chacón Fallas <rafangel1228@gmail.com>
2015-06-22 10:31:10 -0600

    modificaciones menores

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,26 @@
 public class Main {
 
     public static void main(String args[]) throws Exception {
-        Server go = new Server();
-        go.startServer();
+        
+        
+        /*Server go = new Server();
+        go.startServer();*/
+        Facade facade = new Facade();
+       // facade.leerGramatica();
+        //facade.getPrimeroDer(null)
+//        facade.insertarHeaders();
+//        facade.calcularPNR();
+//        ListaString a = facade.getResumenTablaPNR();
+//        System.out.println("looooooooooooooool");
+//        for(int i=0;i<a.getTam();i++){
+//            System.out.println(a.buscarPos(i));
+//        }
+//        System.out.println("looooooooooooooool");
+        
+//        facade.llenarTablaExcel("tablaExcel");
+        //facade.getTabla().getHead().
+        //facade.AnalisisSintactico("declarar int contador newline contador = 5 newline mover 2 contador newline");
+        //facade.AnalisisSemantico("declarar int contador newline contador = 5 newline mover 2 contador newline");
         
     }
 }
