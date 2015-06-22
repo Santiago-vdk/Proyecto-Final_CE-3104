8d7b81a21718f12e67fb113bcf9a966499956d64
victor <victor@victorPC>
2015-06-22 17:55:54 -0600

    Cambios ne el semantico

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,9 @@ public class Main {
         /*Server go = new Server();
         go.startServer();*/
         Facade facade = new Facade();
-       // facade.leerGramatica();
+        facade.AnalisisSemantico("declarar int num1 newline declarar const int num2 newline declarar const int num3 newline declarar string cadena newline declarar char caracter newline num1 = 3 newline num2 = 5 newline num3 = num1 + num2 newline si ( num3 < 9 ) entonces mover 0 5 newline ");
+
+// facade.leerGramatica();
         //facade.getPrimeroDer(null)
 //        facade.insertarHeaders();
 //        facade.calcularPNR();
