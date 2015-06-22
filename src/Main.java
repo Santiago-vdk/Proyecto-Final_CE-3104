9221f3eba24f873a5b034bb7167c67ff7ab67b82
Rafael Chacón Fallas <rafangel1228@gmail.com>
2015-06-22 11:52:47 -0600

    pruebas

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,20 @@ public class Main {
         /*Server go = new Server();
         go.startServer();*/
         Facade facade = new Facade();
-       // facade.leerGramatica();
-        //facade.getPrimeroDer(null)
-//        facade.insertarHeaders();
-//        facade.calcularPNR();
-//        ListaString a = facade.getResumenTablaPNR();
-//        System.out.println("looooooooooooooool");
-//        for(int i=0;i<a.getTam();i++){
-//            System.out.println(a.buscarPos(i));
-//        }
+        facade.leerGramatica();
+   //     ListaString a =facade.getSiguiente("ExpresionMulPrima");
+        facade.insertarHeaders();
+        facade.calcularPNR();
+        ListaString a = facade.getResumenTablaPNR();
+        System.out.println("looooooooooooooool");
+        for(int i=0;i<a.getTam();i++){
+            System.out.println(a.buscarPos(i));
+        }
 //        System.out.println("looooooooooooooool");
         
-//        facade.llenarTablaExcel("tablaExcel");
+        facade.llenarTablaExcel("tablaExcel");
         //facade.getTabla().getHead().
-        //facade.AnalisisSintactico("declarar int contador newline contador = 5 newline mover 2 contador newline");
+        facade.AnalisisSintactico("declarar int contador newline contador = 5 newline mover 2 contador newline newline");
         //facade.AnalisisSemantico("declarar int contador newline contador = 5 newline mover 2 contador newline");
         
     }
