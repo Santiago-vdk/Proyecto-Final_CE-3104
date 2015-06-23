00bbddd6814ec8a12615768ff3c0388332e7d449
Santiago-vdk <Santiago.vdk@gmail.com>
2015-06-22 18:52:14 -0600

    Agrego certificados finales

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String args[]) throws Exception {
         
         
-        /*Server go = new Server();
-        go.startServer();*/
-        Facade facade = new Facade();
+        Server go = new Server();
+        go.startServer();
+       /* Facade facade = new Facade();
         facade.AnalisisSemantico("declarar int num1 newline declarar const int num2 newline declarar const int num3 newline declarar string cadena newline declarar char caracter newline num1 = 3 newline num2 = 5 newline num3 = num1 + num2 newline si ( num3 < 9 ) entonces mover 0 5 newline ");
 
 // facade.leerGramatica();
@@ -27,6 +27,6 @@ public class Main {
         facade.AnalisisSintactico("declarar int contador newline declarar const char abeja  newline mover 2 contador newline contador = contador * 10 newline mover 3 3 newline haga casa = casa / 2 newline mientras ( 4 == casa ) newline ");
         
         //facade.AnalisisSemantico("declarar int contador newline contador = 5 newline mover 2 contador newline");
-        
+     */   
     }
 }
